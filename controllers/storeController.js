08195baf74333a118af1cb32d973b64f43575c19
Luis Amez <luis.amez.vitoria@gmail.com>
2017-09-09 18:17:25 +0200

    Add edit option for stores

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -6,16 +6,37 @@ exports.homePage = (req, res) => {
 };
 
 exports.addStore = (req, res) => {
-  res.render("editStore", {title: "Add Store"});
+  res.render("editStore", { title: "Add Store" });
 };
 
 exports.createStore = async (req, res) => {
   const store = await (new Store(req.body)).save();
-  req.flash("success", `Successfully created ${store.name}. Care to leave a review?`);
+  req.flash("success", `Successfully created <strong>${store.name}</strong>. Care to leave a review?`);
   res.redirect(`/store/${store.slug}`);
 };
 
 exports.getStores = async (req, res) => {
+  // 1. Query the database for a list of all stores
   const stores = await Store.find();
   res.render("stores", { title: "Stores", stores });
-}
+};
+
+exports.editStore = async (req, res) => {
+  // 1. Find the store given the ID
+  const store = await Store.findOne({ _id: req.params.id });
+  // 2. Confirm the user is the store's owner
+  // TODO
+  // 3. Render out the edit form
+  res.render("editStore", { title: `Edit ${store.name}`, store });
+};
+
+exports.updateStore = async (req, res) => {
+  // 1. Find and update the store
+  const store = await Store.findOneAndUpdate({ _id: req.params.id }, req.body, { 
+    new: true, // return the new store instead of the old one
+    runValidators: true // by default, validators only run on creation
+  }).exec();
+  // 2. Redirect them to the store and tell it worked
+  req.flash("succes", `Successfully updated <strong>${store.name}</strong>. <a href="/stores/${store.slug}">View Store</a>`);
+  res.redirect(`/stores/${store._id}/edit`);
+};
