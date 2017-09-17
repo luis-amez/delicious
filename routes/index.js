23133dbd089c03a8254639e07d4922bb9eedd032
Luis Amez <luis.amez.vitoria@gmail.com>
2017-09-17 18:40:16 +0200

    Add register validation middleware

--- routes/index.js
+++ routes/index.js
@@ -24,5 +24,6 @@ router.get("/tags", catchErrors(storeController.getStoresByTag));
 router.get("/tags/:tag", catchErrors(storeController.getStoresByTag));
 router.get("/login", userController.loginForm);
 router.get("/register", userController.registerForm);
+router.post("/register", userController.validateRegister);
 
 module.exports = router;
