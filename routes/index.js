7034d40171ad5a2d70af6fab891f0576b55ff23d
Luis Amez <luis.amez.vitoria@gmail.com>
2017-11-19 19:51:10 +0100

    Add update profile

--- routes/index.js
+++ routes/index.js
@@ -36,5 +36,7 @@ router.post("/register",
   authController.login
 );
 router.get("/logout", authController.logout);
+router.get("/account", authController.isLoggedIn, userController.account);
+router.post("/account", catchErrors(userController.updateAccount));
 
 module.exports = router;
